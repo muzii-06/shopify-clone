f4bb7a8efc91e840473e6cdcb601fc9eb2304133
Muzammil's PC <ma.muzammilarif@gmail.com>
2025-02-25 18:56:37 +0500

    update commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,13 +6,14 @@ import HomePage from './HomePage'
 import TopCategories from './TopCategories'
 import CardContainer from './CardContainer'
 import TrendingProducts from './TrendingProducts'
-// import YouTubee from './Youtubee'
+
 import CustomerReviews from './CustomerReviews'
 import BrandLogos from './BrandLogos'
 import Banner from './Banner'
 import Features from './Features'
 import Articles from './Articles'
 import Footer from './Footer'
+import Vid from './Vid'
 
 const Home = () => {
   return (
@@ -24,7 +25,7 @@ const Home = () => {
     <TopCategories />
     <CardContainer />
     <TrendingProducts />
-    {/* <YouTubee videoId="9no7Q4jYnsI" /> */}
+    <Vid videoId="9no7Q4jYnsI" />
     <CustomerReviews />
     <BrandLogos />
     <Banner />
